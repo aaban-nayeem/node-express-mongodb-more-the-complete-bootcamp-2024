b4364b3625e003999bfde6e0c2fa0f511ebc9a9a
Aaban Nayeem <aabannayeem4358@gmail.com>
2024-06-13 11:16:21 +0600

    Here, ready all setup folder/file for starting project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import morgan from "morgan"
+import express from "express"
+const app = express()
+
+app.use(morgan("combined"))
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "Hello from server side",
+    app: "Natours",
+  })
+})
+
+app.post("/", (req, res) => {
+  res.send("You can now post this routes")
+})
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000")
+})
