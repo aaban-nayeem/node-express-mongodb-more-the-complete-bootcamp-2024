4150f4c6ed5ec8e322f3c2aae7b0830a01cb8e7d
Aaban Nayeem <aabannayeem4358@gmail.com>
2024-06-14 17:31:25 +0600

    Resourse collection

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ const app = express()
 
 app.use(morgan("combined"))
 
-app.get("/", (req, res) => {
-  res.status(200).send({
-    message: "Hello from server side",
-    app: "Natours",
-  })
-})
+// app.get("/", (req, res) => {
+//   res.status(200).send({
+//     message: "Hello from server side",
+//     app: "Natours",
+//   })
+// })
 
-app.post("/", (req, res) => {
-  res.send("You can now post this routes")
-})
+// app.post("/", (req, res) => {
+//   res.send("You can now post this routes")
+// })
+
+app.get("/api/v1/tours", (req, res) => {})
 
 app.listen(3000, () => {
   console.log("Listening on port 3000")
